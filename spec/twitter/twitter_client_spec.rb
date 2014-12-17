a3af552ebbd6cbf3a00bca3c878ad28b9ceea860
Kavita <okavita@gmail.com>
2014-12-17 14:29:04 -0700

    refactored test file for VCR rate limit errors

--- spec/twitter/twitter_client_spec.rb
+++ spec/twitter/twitter_client_spec.rb
@@ -4,36 +4,35 @@ describe TwitterClient do
   let(:client) { TwitterClient.new }
 
   context 'tweets' do
-    before do
+    let (:tweets) { client.fetch_tweets('j3') }
+
+    it 'fetches array of tweets' do
       VCR.use_cassette("j3_tweets") do
-        @tweets = client.fetch_tweets('j3')
+        expect(tweets).to be_a Array
       end
     end
 
-    it 'fetches array of tweets' do
-      expect(@tweets).to be_a Array
-    end
-
     it 'returns tweet objects with text' do
-      expect(@tweets.first.text?).to eq true
+      VCR.use_cassette("j3_tweets") do
+        expect(tweets.first.text?).to eq true
+      end
     end
   end
 
   context 'mentions' do
-    before do
+    let (:mentions) { client.fetch_mentions }
+
+    it 'fetches array of mentions' do
       VCR.use_cassette("j3_mentions") do
-        @mentions = client.fetch_mentions
+        expect(mentions).to be_a Array
       end
     end
 
-    it 'fetches array of mentions' do
-      expect(@mentions).to be_a Array
-    end
-
     it 'returns mention objects with text' do
-      expect(@mentions.first.text?).to eq true
+      VCR.use_cassette("j3_mentions") do
+        expect(mentions.first.text?).to eq true
+      end
     end
   end
-
 
 end
