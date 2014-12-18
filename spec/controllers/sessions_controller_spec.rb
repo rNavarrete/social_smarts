93431c20155c2d9ceac3f0385c8f28ef0a7cc6c8
Luke Aiken <lukeaiken@gmail.com>
2014-12-18 09:26:38 -0700

    moving toward controller tests, away from capybara features.

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe SessionsController, type: :controller do
+  before do
+    request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+  end
+    
+  describe "POST create" do
+    it "sets the user_id in the session" do
+      post :create
+      expect(session[:user_id]).to be
+    end
+
+    it "should redirect the user to the root" do
+      post :create
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe "DELETE destroy" do
+    before do
+      post :create
+    end
+
+    it "should clear the session" do
+      expect(session[:user_id]).to be
+      delete :destroy
+      expect(session[:user_id]).to be_nil
+    end
+
+    it "should redirect to the root" do
+      delete :destroy
+      expect(response).to redirect_to root_path
+    end
+  end
+
+end
