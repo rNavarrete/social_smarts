e194c47990481fa32e307eff0223482756a29507
Luke Aiken <lukeaiken@gmail.com>
2014-12-21 12:32:27 -0700

    update oath tokens upon login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_secret = auth.credentials.secret
-      user.save!
-    end
+    user = where(provider: auth.provider, uid: auth.uid).first
+    user ? user.update_auth_attrs(auth) : create do |user|
+                                            user.provider = auth.provider
+                                            user.uid = auth.uid
+                                            user.name = auth.info.name
+                                            user.oauth_token = auth.credentials.token
+                                            user.oauth_secret = auth.credentials.secret
+                                            user.save!
+                                          end
   end
   
   def client
     Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV["twitter_consumer_key"]
-    config.consumer_secret     = ENV["twitter_api_secret_key"]
-    config.access_token        = oauth_token
-    config.access_token_secret = oauth_secret
+      config.consumer_key        = ENV["twitter_consumer_key"]
+      config.consumer_secret     = ENV["twitter_api_secret_key"]
+      config.access_token        = oauth_token
+      config.access_token_secret = oauth_secret
     end
   end
 
@@ -29,5 +30,15 @@ class User < ActiveRecord::Base
 
   def fetch_mentions
     client.mentions_timeline
+  end
+
+  def update_auth_attrs(auth)
+    provider = auth.provider
+    uid = auth.uid
+    name = auth.info.name
+    oauth_token = auth.credentials.token
+    oauth_secret = auth.credentials.secret
+    save!
+    self
   end
 end
