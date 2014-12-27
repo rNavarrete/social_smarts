67627ab1e03e9521d41e92ef1b34944db57729a4
Glen Egbert <glenegbert1@gmail.com>
2014-12-27 16:05:38 -0700

    added controller test for twitter mentions controller

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -5,9 +5,7 @@ angular.module('socialsmartsApp.controllers', [])
   });
   $http.get('/twitter_location.json').success(function(data) {
     var locale = data;
-
-
-  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
+  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=' + 'AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
   $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
     console.log(locale[0])
   });
