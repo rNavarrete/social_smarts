40c3c57f8f018936fded49c7a136c24e5d9cafae
Glen Egbert <glenegbert1@gmail.com>
2014-12-27 14:57:37 -0700

    basic map works via google angular api, map centers basaed on locaation defined in user's profile

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -4,11 +4,14 @@ angular.module('socialsmartsApp.controllers', [])
     $scope.timeline = data;
   });
   $http.get('/twitter_location.json').success(function(data) {
-    $scope.locale = data;
-    console.log(data)
-  });
-  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    var locale = data;
 
+
+  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
+  $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
+    console.log(locale[0])
+  });
+});
   // TimelineService.getTimeline()
   // .then(function(data) {
   //   $scope.timeline = data;
