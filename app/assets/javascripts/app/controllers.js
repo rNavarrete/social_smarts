d5055143986781a7d0f40b54c0218c585d9d3eab
Glen Egbert <glenegbert1@gmail.com>
2014-12-27 14:16:05 -0700

    basic map set up using google angular maps directive

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,10 +1,14 @@
 angular.module('socialsmartsApp.controllers', [])
 .controller('DashboardController', function($scope, $http) {
   $http.get('/twitter_timeline.json').success(function(data) {
-    console.log(data)
     $scope.timeline = data;
   });
-  
+  $http.get('/twitter_location.json').success(function(data) {
+    $scope.locale = data;
+    console.log(data)
+  });
+  $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
   // TimelineService.getTimeline()
   // .then(function(data) {
   //   $scope.timeline = data;
