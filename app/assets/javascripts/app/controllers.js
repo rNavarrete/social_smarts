f0dadddd8efba14e5dcf6b90bd12a7c5a97c2000
Luke Aiken <lukeaiken@gmail.com>
2014-12-28 13:47:27 -0700

    move javascript libraries out of app folder

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -8,12 +8,8 @@ angular.module('socialsmartsApp.controllers', [])
 
 
   $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
-  $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
-    console.log(locale[0])
+    $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
+      console.log(locale[0])
+    });
   });
-});
-  // TimelineService.getTimeline()
-  // .then(function(data) {
-  //   $scope.timeline = data;
-  // });
 });
