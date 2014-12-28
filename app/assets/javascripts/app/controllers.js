c6df58dea816f3bb17261f5c8a42e9ba8ad8b5dd
Glen Egbert <glenegbert1@gmail.com>
2014-12-27 18:09:35 -0700

    WIP problems interpelating map marker coordinates

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -3,13 +3,24 @@ angular.module('socialsmartsApp.controllers', [])
   $http.get('/twitter_timeline.json').success(function(data) {
     $scope.timeline = data;
   });
+
   $http.get('/twitter_location.json').success(function(data) {
     var locale = data;
-  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=' + 'AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
-  $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
-    console.log(locale[0])
+    $http.get('/twitter_mentions.json').success(function(atmentions) {
+      $scope.mentions = []
+      for (var i = 0; i < atmentions.length; i++) {
+        if (atmentions[i].tweet.place) {
+          $scope.mentions.push({id: atmentions[i].tweet.id, coords: {latitude: atmentions[i].tweet.place.bounding_box.coordinates[0][0][0],
+            longitude: atmentions[i].tweet.place.bounding_box.coordinates[0][0][1]}
+          });
+        }
+      }
+    console.log($scope.mentions)
+    });
+    $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + locale[0] + '&key=' + 'AIzaSyCMPvf6SDEQMMwrlpu1jp9hz_F5XdV4RaE').success(function(data) {
+    $scope.map = { center: { latitude: data.results[0].geometry.location.lat, longitude: data.results[0].geometry.location.lng }, zoom: 8 };
+    });
   });
-});
   // TimelineService.getTimeline()
   // .then(function(data) {
   //   $scope.timeline = data;
