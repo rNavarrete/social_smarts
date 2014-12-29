24cc143d5dd1db437b9f0f806119bdb7c5504372
Luke Aiken <lukeaiken@gmail.com>
2014-12-29 16:07:22 -0700

    fix tests for using jbuilder

--- spec/models/tracked_tweets_spec.rb
+++ spec/models/tracked_tweets_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe TrackedTweets, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe TrackedTweet, :type => :model do
 end
