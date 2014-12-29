a46a3e1657738fa4267994248b8661f1c0d0c71a
Glen Egbert <glenegbert1@gmail.com>
2014-12-29 16:20:13 -0700

    tweet feedback working and disappearing after 1sec.

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,5 +1,5 @@
 angular.module('socialsmartsApp.controllers', [])
-.controller('DashboardController', function($scope, $http) {
+.controller('DashboardController', function($scope, $http, $interval) {
   $scope.tweet_message = "";
 
   $scope.disabled = function(tweet_message) {
@@ -48,28 +48,32 @@ angular.module('socialsmartsApp.controllers', [])
   $scope.sendTweet = function(tweet_message) {
     $http.post('/twitter_timeline.json', {tweet: tweet_message})
     .success(function(data, status, headers, config) {
-      if (data.status == 'OK') {
+      if (data.status === 'ok') {
         $scope.tweet_message = null;
-        // $scope.messages = 'Your form has been sent!';
+
+        $scope.messages = 'Your form has been sent!';
         // $scope.submitted = false;
       } else {
-        // $scope.messages = 'Oops, we received your request, but there was an error processing it.';
-        $log.error(data);
+        $scope.messages = 'Oops, we received your request, but there was an error processing it.';
+        // $log.error(data);
       }
     })
     .error(function(data, status, headers, config) {
       $scope.tweet_message = null;
 
       // $scope.progress = data;
-      // $scope.tweet_message = 'There was a network error. Try again later.';
-      $log.error(data);
+      $scope.messages = 'There was a network error. Try again later.';
+      // $log.error(data);
     })
-    // .finally(function() {
-    //   // Hide status messages after three seconds.
-    //   $timeout(function() {
-    //     $scope.messages = null;
-    //   }, 3000);
-    // });
+    .finally(function() {
+      // Hide status messages after three seconds.
+      $interval(function() {
+        $scope.messages = null;
+      }, [1000]);
+      // setTimeout(function() {
+        // console.log($scope)
+      // }, 1000);
+    });
   }
 
 });
