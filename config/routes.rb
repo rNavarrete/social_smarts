8169507be6402f06757a9fb353838506d161f4bc
Glen Egbert <glenegbert1@gmail.com>
2014-12-30 07:20:44 -0700

    set up and tested (manually) scheduledtweets controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :twitter_timeline, only: [:index, :create]
   resources :twitter_mentions, only: [:index]
   resources :twitter_location, only: [:index]
+  resources :scheduled_tweet, only: [:create]
 
   get "/login" => redirect("/auth/twitter"), as: :login
   get '/auth/twitter/callback', to: 'sessions#create'
