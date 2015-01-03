36144874a81685aa82368671da20359c42c26b34
Luke Aiken <lukeaiken@gmail.com>
2015-01-03 09:15:53 -0700

    unit testing for tracked tweet service

--- spec/javascripts/services_spec.js
+++ spec/javascripts/services_spec.js
@@ -1,22 +1,55 @@
 (function() {
   'use strict';
 
-  describe('Service: pollingService', function() {
+  describe('socialsmartsApp.services', function() {
+    var tweets, $httpBackend, pollingService, TrackedTweet,
+        xyzTweetData = function() {
+          return [{text: "post1", screen_name: "luke", created_at: "2014", klout_score: 20, status: "unresolved"},
+                  {text: "post2", screen_name: "kavita", created_at: "2015", klout_score: 30, status: "unresolved"}]
+        };
+
     beforeEach(module('socialsmartsApp.services'));
+      beforeEach(inject(function(_$httpBackend_, _pollingService_) {
+        $httpBackend = _$httpBackend_;
+        pollingService = _pollingService_;
+      }));
 
-    it('returns a JSON object with tweets', inject(function ($httpBackend, pollingService) {
-      var tweets;
+    describe('Service: pollingService', function() {
+      it('returns a JSON object with tweets', inject(function (_$httpBackend_, _pollingService_) {
+        $httpBackend.when('GET', '/twitter_timeline.json').respond(xyzTweetData());
+        pollingService.startPolling('timeline', '/twitter_timeline.json', 60000, function(resp) {
+          tweets = resp.data;
+        });
+        $httpBackend.flush();
+        expect(tweets).toEqual(xyzTweetData());
+      }));
+    });
 
-      $httpBackend.when('GET', '/twitter_timeline.json').respond([{text: "post1", screen_name: "luke", created_at: "2014"},
-                                                                  {text: "post2", screen_name: "kavita", created_at: "2015"}]);
+    describe('Service: TrackedTweet', function() {
+      beforeEach(inject(function(_$httpBackend_, _TrackedTweet_) {
+        $httpBackend = _$httpBackend_;
+        TrackedTweet = _TrackedTweet_;
+      }));
 
-      pollingService.startPolling('timeline', '/twitter_timeline.json', 60000, function(resp) {
-        tweets = resp.data;
+      it('returns a JSON object with tracked tweets', function() {
+        $httpBackend.when('GET', '/tracked_tweets.json').respond(xyzTweetData());
+        TrackedTweet.query(function(resp) {
+          tweets = resp;
+        });
+        $httpBackend.flush();
+        expect(angular.equals(tweets, xyzTweetData())).toEqual(true);
       });
 
-      $httpBackend.flush();
-      expect(tweets).toEqual([{text: "post1", screen_name: "luke", created_at: "2014"},
-                              {text: "post2", screen_name: "kavita", created_at: "2015"}]);
-    }));
+      it("updates a tracked tweet's status", function() {
+        $httpBackend.when('PATCH', '/tracked_tweets/1.json').respond({text: "post1", status: "resolved"});
+        var tracked_tweet = new TrackedTweet({
+          text: "post1", 
+          status: "resloved"
+        });
+        tracked_tweet.$update({id: 1});
+        $httpBackend.flush();
+        expect(angular.equals(tracked_tweet, {text: "post1", status: "resolved"})).toEqual(true);
+      });
+    });
   });
 })();
