0fd5eb6499afcfa053d26c4a3137ce575a297b84
Luke Aiken <lukeaiken@gmail.com>
2015-01-04 10:28:06 -0700

    tracked tweet model specs

--- spec/models/tracked_tweets_spec.rb
+++ spec/models/tracked_tweets_spec.rb
@@ -1,4 +1,17 @@
-require 'rails_helper'
-
 RSpec.describe TrackedTweet, :type => :model do
+  let(:user) { create(:user) }
+  let(:tracked_tweet) { create(:tracked_tweet, user: user) }
+
+  it "belongs to a user" do
+    expect(tracked_tweet.user).to eq user
+  end
+
+  it "has a status as either resolved or unresolved" do
+    tracked_tweet.status = "resolved"
+    expect(tracked_tweet).to be_valid
+    tracked_tweet.status = "unresolved"
+    expect(tracked_tweet).to be_valid
+    tracked_tweet.status = ""
+    expect(tracked_tweet).to_not be_valid
+  end
 end
