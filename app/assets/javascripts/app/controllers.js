88cdc4077625604c3c6eddc208ab17e61abc3bcc
Edgard Navarrete <kinglouis.belmont@gmail.com>
2015-01-07 00:17:14 -0700

    added new color scheme for map

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -21,7 +21,11 @@ angular.module('socialsmartsApp.controllers', [])
     $scope.current_user_klout = resp.data.current_user_klout;
   });
 
+
+
   $scope.orderProp = '-klout_score';
+  $scope.options = {styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]}
+
 
   $scope.tweet_message = "";
   var today = new Date();
