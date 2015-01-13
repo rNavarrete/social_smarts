452b24d3f5a4e5aa8f0fdf97fdcc1185561b35fc
Luke Aiken <lukeaiken@gmail.com>
2015-01-12 19:34:11 -0700

    move params to private

--- app/controllers/tracked_tweets_controller.rb
+++ app/controllers/tracked_tweets_controller.rb
@@ -7,15 +7,6 @@ class TrackedTweetsController < ApplicationController
   end
 
   def create
-    tracked_tweet_params = {
-      text: params[:text],
-      screen_name: params[:screen_name],
-      created_at: params[:created_at],
-      klout_score: params[:klout_score],
-      status: params[:status],
-      image: params[:image]
-    }
-
     tracked_tweet = current_user.tracked_tweets.new(tracked_tweet_params)
     tracked_tweet.save
 
@@ -32,4 +23,18 @@ class TrackedTweetsController < ApplicationController
   def destroy
     respond_with current_user.tracked_tweets.destroy(params[:id])
   end
+
+  private
+
+    def tracked_tweet_params
+      {
+        text: params[:text],
+        screen_name: params[:screen_name],
+        created_at: params[:created_at],
+        klout_score: params[:klout_score],
+        status: params[:status],
+        image: params[:image]
+      }
+    end
+
 end
